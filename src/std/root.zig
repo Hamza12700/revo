ef0f805ac98c8f2623ae7d9f96059124f696a5ee
lung <62560250+if-not-nil@users.noreply.github.com>
2026-06-03 17:22:15 +0300

    fixup pr
    this applies to https://codeberg.org/lung/revo/pulls/6

--- src/std/root.zig
+++ src/std/root.zig
@@ -1036,8 +1036,8 @@ pub fn read(args: []const Data, vm: *VM) !NativeResult {
     var w = std.Io.Writer.Allocating.init(vm.runtime.alloc);
     defer w.deinit();
 
-    _ = r.interface.streamDelimiter(&w.writer, delimiter) catch
-        return resultTuple(vm, .err, try vm.ownDataString("EndOfStream"));
+    _ = r.interface.streamDelimiter(&w.writer, delimiter) catch |e|
+        return .Err(vm, @errorName(e));
     const result_str = try w.toOwnedSlice();
     return resultTuple(vm, .ok, try vm.adoptDataString(result_str));
 }
